a96074a0434e805359946a23019c4541cef65952
Fred the Ninja <fredtheninja@icloud.com>
2021-01-04 16:34:34 +0100

    Day 1 - Intermeditate

---
+++ Day 1/Day 1.2 Debugging Exercise/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+#Fix the code below 👇
+
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the \"+\" sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+#Original with errors
+#print(Day 1 - String Manipulation")
+#print("String Concatenation is done with the "+" sign.")
+#  print('e.g. print("Hello " + "world")')
+#print(("New lines can be created with a backslash and n.")
---
+++ Day 1/main.py
@@ -0,0 +1,17 @@
+#Day 1 - Basic stuff
+
+#print("Hello World!")
+
+#Coding Challenge - See readme.md
+print("Coding Challenge - Output:")
+print("Day 1 - Python Print Function")
+print("This function is declared like this:")
+print("print('what to print')")
+
+#Lesson 2 - String Manipulation
+print("Lesson 2 - String Manipulation:")
+#New Line
+print("Hello world!\nHello World!")
+#String concatenation
+print("Hello"+" "+"FredTheNinja")
+
