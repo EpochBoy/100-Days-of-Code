d7a8c5d8405312019d4ddab03ceec4e5217544d2
Fred the Ninja <fredtheninja@icloud.com>
2021-01-05 22:49:17 +0100

    Day2_Exc1_Done

---
+++ Day2/Exc1/main.py
@@ -0,0 +1,12 @@
+a = input("Type a 2 digit number: ")
+
+#My Solution
+print(int(a[0])+int(a[1]))
+
+#Facit
+first_digit = a[0]
+second_digit = a[1]
+
+result = int(first_digit)+int(second_digit)
+print(result)
+
--- Day2/main.py
+++ Day2/main.py
@@ -1,6 +1,34 @@
-#Data types
+#Basic Data types
 
 #String
 #This method of pulling out a char from String is called Subscripting
 print("Hello"[4])
 
+print("123"+"456")
+
+#Integer
+print(123+456)
+
+#Delimiters
+print(123_456_789)
+
+#Float
+print(3.14159)
+
+#Boolean
+print(True)
+print(False)
+
+num_char = len(input("What is your name? "))
+
+#checking var type
+print(type(num_char))
+
+#typecasting
+new_num_char = str(num_char)
+
+#Now no typeclashing
+print("Your name has "+new_num_char+" characters")
+
+a = str(123)
+print(type(a))
