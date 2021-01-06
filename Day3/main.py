37f180d7d96a482bbf96437382adeb97905ae97a
FredTheNinja <fredtheninja@icloud.com>
2021-01-06 14:18:35 +0100

    Day3_Exc1_Done

--- Day3/main.py
+++ Day3/main.py
@@ -1,1 +1,27 @@
-print("gittest mbp")
+#if else statements
+
+# water_level = 90
+
+# if water_level > 80:
+#     print("Drain Water")
+# else:
+#     print("Fill bathtub")
+
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? "))
+
+# if height >= 120:
+#     print("You can ride the coaster!")
+# else:
+#     print("Sorry, please come back when you've grown")
+
+#Comparison Operators
+# > Greater than
+# < Less than
+# >= Greater than or equal to
+# <= Less than or equal to
+# == Equal to
+# != Not equal to
+
+
+
