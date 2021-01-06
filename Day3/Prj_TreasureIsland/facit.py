d6eb3df43a3381893f3f3532dc7e6c065accd1f0
FredTheNinja <fredtheninja@icloud.com>
2021-01-06 17:41:07 +0100

    Day3 Project Done

--- Day3/Prj_TreasureIsland/facit.py
+++ Day3/Prj_TreasureIsland/facit.py
@@ -22,29 +22,20 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 ''')
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
-
-xroad = input("You're at a xroad, do you want to go left or right? Type left or right\n").lower()
-if xroad == "left":
-    print("You are indeed an experienced adventurer!\n")
-    lake = input("You've now come to a lake, want to swim across or go around it? Type swim or around\n").lower()
-    if lake == "around":
-        print("You are tired af the long walk, but it seems your choice was a good one.\n")
-        print("You spotted several crocodiles in the lake during your walk\n")
-        print("You've arrived at the castle, how will this challenge be tackled?\n")
-        print("It seems you have several choices. A red door, a blue door or the yellow door\n")
+choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
+if choice1 == "left":
+  choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
+  if choice2 == "wait":
+    choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
+    if choice3 == "red":
+      print("It's a room full of fire. Game Over.")
+    elif choice3 == "yellow":
+      print("You found the treasure! You Win!")
+    elif choice3 == "blue":
+      print("You enter a room of beasts. Game Over.")
     else:
-        print("You are devoured by hungry crocodiles! Poor Boo, who will save him now?")
-        exit()
-    door = input("Type red, blue or yellow, to pick a door\n").lower()
-    if door == "yellow":
-        print("Congratulations Minsc, you've saved Boo")
-    elif door == "red":
-        print("Poor Minsc, you've been burned to a crisp")
-    elif door == "blue":
-        print("Irenicus transforms Boo into a dragon, that kills you")
+      print("You chose a door that doesn't exist. Game Over.")
+  else:
+    print("You get attacked by an angry trout. Game Over.")
 else:
-    print("What a poor choice you've made. You are now DEAD")
-
-
-
-#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+  print("You fell into a hole. Game Over.")
---
+++ Day3/Prj_TreasureIsland/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+if door == "red":
+        print("Poor Minsc, you've been burned to a crisp")
+    if door == "blue":
+        print("Irenicus transforms Boo into a dragon, that kills you")
