e7200712ea6b5177ee42215f749ae46710b1e58c
Fred the Ninja <fredtheninja@icloud.com>
2021-01-08 04:03:34 +0100

    Day4 Exc1 Done

---
+++ Day4/Exc1/main.py
@@ -0,0 +1,17 @@
+import random
+
+Heads_Tails = round(random.random())
+
+if Heads_Tails == 1:
+    print("Heads")
+else:
+    print("Tails")
+
+#Facit
+
+random_side = random.randint(0,1)
+
+if random_side == 1:
+    print("Heads")
+else:
+    print("Tails")
--- Day4/main.py
+++ Day4/main.py
@@ -1,6 +1,15 @@
 import random
+import my_module
 
 random_integer = random.randint(1, 10)
 print(random_integer)
 
- 
+print(my_module.pi)
+
+random_float = random.random()+random.randint(0,5)
+print(random_float)
+random_float2 = random.random()*5
+print(random_float2)
+
+love_score = random.randint(1,100)
+print(f"You love score is {love_score}")
