ecc7e27d5d654ec5ccefe583b8287ee1b23bc35c
Fred the Ninja <fredtheninja@icloud.com>
2021-01-12 12:48:03 +0100

    Day6 Reeborg Maze done

--- Day6/main.py
+++ Day6/main.py
@@ -37,3 +37,64 @@ def hurdle():
 
 for i in range(0,6):
     hurdle()
+
+#Hurdle 2, random flag
+
+while at_goal() != 1:
+    hurdle()
+
+#Hurdle 2, Random flag facit
+while not at_goal():
+    hurdle()
+
+#Hurdle 3, Random Wall placement
+def hurdle2():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+while not at_goal():
+    if wall_in_front():
+        hurdle2()
+    elif front_is_clear():
+        move()
+
+#Hurdle 4, Variable Height hurdle
+def hurdle_top():
+    turn_right()
+    move()
+    turn_right()
+    move()
+
+while not at_goal():
+    if wall_in_front():
+        turn_left()
+        while wall_on_right() and front_is_clear():
+            if at_goal() != 1:
+                move()
+        if right_is_clear():
+            hurdle_top()
+    elif front_is_clear:
+        move()
+    else:
+        turn_left()
+
+#Reeborg Maze
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif wall_on_right() and front_is_clear():
+        move()
+    else:
+        turn_left()
