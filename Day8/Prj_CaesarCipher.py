9f60a36c60596ff13792932a90f9f880c4af1292
Fred the Ninja <fredtheninja@icloud.com>
2021-01-21 01:01:49 +0100

    Caesar Part1 Done

--- Day8/Prj_CaesarCipher.py
+++ Day8/Prj_CaesarCipher.py
@@ -1,4 +1,5 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+#added alphabet twice to list to avoid index out of bounds
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
@@ -11,11 +12,6 @@ textlist = []
 def convert_string(text):
     for i in text:
         textlist.append(i)
-
-def encrypt(text, shift):
-    for eac
-        textlist.append(i)
-
 
 
     #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
@@ -30,6 +26,34 @@ def encrypt(text, shift):
 
     ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
+#Fails on Z and doubling alphabet list breaks logic
+# def encrypt(text, shift):
+#     convert_string(text)
+#     for i in textlist:
+#         for j in alphabet:
+#             if i == j:
+#                 temp1 = alphabet.index(j)+shift
+#                 templetter1 = alphabet[temp1]
+#                 temp2 = textlist.index(i)
+#                 textlist[temp2] = templetter1
+#     final_string = "".join(textlist)
+#     print(f"The encrypted message is: {final_string}")
+
+
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
 
-encrypt(text, shift)
+# encrypt(text, shift)
+
+#facit#facit#facit#facit#facit#facit#facit#facit#facit#facit#facit#facit#facit#facit#facit#facit#facit
+
+# facit part 1
+def encrypt(plain_text, shift_amount):
+    cipher_text = ""
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
+    print(f"The encoded text is {cipher_text}")
+
+encrypt(plain_text=text, shift_amount=shift)
