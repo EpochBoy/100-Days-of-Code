28a0096d03d514c94e1d92870e6f20bfca5b1dd5
Fred the Ninja <fredtheninja@icloud.com>
2021-01-21 02:17:44 +0100

    Day8 CaesarFinal Done

---
+++ Day8/Prj_CaesarFinal.py
@@ -0,0 +1,50 @@
+
+#TODO-1: Import and print the logo from art.py when the program starts.
+
+#TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
+#Try running the program and entering a shift number of 45.
+#Hint: Think about how you can use the modulus (%).
+
+#TODO-3: What happens if the user enters a number/symbol/space?
+#Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
+#e.g. start_text = "meet me at 3"
+#end_text = "•••• •• •• 3"
+
+#TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
+#e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
+#If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
+#Hint: Try creating a new function that calls itself if they type 'yes'. 
+
+from art import logo
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+state = ""
+
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char not in alphabet:
+        end_text += char
+    elif char in alphabet:
+        position = alphabet.index(char)
+        new_position = position + shift_amount
+        end_text += alphabet[new_position]
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+def runner(state):
+    if state == "no":
+        print("Goodbye")
+        exit()
+    else:
+        direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
+        shift = shift % 26
+        caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+        state = input("Type 'yes' if you want to go again. Otherwise type 'no' ")
+        runner(state)
+
+print(logo)
+runner(state)
