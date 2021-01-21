c3d1709867be78785a2610df07988269a054c8f7
Fred the Ninja <fredtheninja@icloud.com>
2021-01-21 01:18:03 +0100

    Day8 CaesarCipher Part2 Done

--- Day8/Prj_CaesarCipher.py
+++ Day8/Prj_CaesarCipher.py
@@ -5,13 +5,72 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
+
+def encrypt(plain_text, shift_amount):
+    cipher_text = ""
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
+    print(f"The encoded text is {cipher_text}")
+
+
+
+
+#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+
+
+  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
+  #e.g. 
+  #cipher_text = "mjqqt"
+  #shift = 5
+  #plain_text = "hello"
+  #print output: "The decoded text is hello"
+
+
+def decrypt(enc_text, rev_shift):
+    dec_text = ""
+    for letter in enc_text:
+        position = alphabet.index(letter)
+        new_position = position-rev_shift
+        new_letter = alphabet[new_position]
+        dec_text += new_letter
+    print(f"The decoded text is {dec_text}")
+
+
+#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'direction' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+
+if direction == "encode":
+    encrypt(plain_text=text, shift_amount=shift)
+elif direction == "decode":
+    decrypt(enc_text=text, rev_shift=shift)
+else:
+    print("Incorrect input")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 
-textlist = []
+# textlist = []
 
-def convert_string(text):
-    for i in text:
-        textlist.append(i)
+# def convert_string(text):
+#     for i in text:
+#         textlist.append(i)
 
 
     #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
@@ -44,16 +103,19 @@ def convert_string(text):
 
 # encrypt(text, shift)
 
+
+
 #facit#facit#facit#facit#facit#facit#facit#facit#facit#facit#facit#facit#facit#facit#facit#facit#facit
 
 # facit part 1
-def encrypt(plain_text, shift_amount):
-    cipher_text = ""
-    for letter in plain_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
-    print(f"The encoded text is {cipher_text}")
+# def encrypt(plain_text, shift_amount):
+#     cipher_text = ""
+#     for letter in plain_text:
+#         position = alphabet.index(letter)
+#         new_position = position + shift_amount
+#         new_letter = alphabet[new_position]
+#         cipher_text += new_letter
+#     print(f"The encoded text is {cipher_text}")
 
-encrypt(plain_text=text, shift_amount=shift)
+# encrypt(plain_text=text, shift_amount=shift)
+
