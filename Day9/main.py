17dd457bc1459d62fe949acacaf149a9c11a8c4a
Fred the Ninja <fredtheninja@icloud.com>
2021-01-21 21:07:13 +0100

    Day9 Exc2 Start

--- Day9/main.py
+++ Day9/main.py
@@ -23,3 +23,47 @@ for key in programmin_dict:
 programmin_dict = {}
 
 print(f"Content of program dict {programmin_dict}")
+
+#Nesting list and dicts
+print("NESTING LISTS AND DICTS BELOW THIS LINE")
+print("=======================================")
+
+capitals = {
+    "France": "Paris",
+    "Denmark": "Copenhagen",
+}
+
+#Nesting a list in a dictionary
+travel_log = {
+    "France": ["Paris", "Lile", "Nancy"],
+    "Denmark": ["Copenhagen", "Odense", "Skanderborg"]
+}
+
+#Nesting a dictionary in a dictionary
+travel_log2 = {
+    "France": {
+        "cities_visited": ["Paris", "Lile", "Nancy"],
+        "total_visits": 12
+        },
+    "Denmark":{
+        "cities_visited": ["Copenhagen", "Odense", "Skanderborg"],
+        "total_visits": 12
+        }
+}
+
+#Nesting a Dictionary inside a list
+
+travel_log3 = [
+    {
+    "country": "France", 
+    "cities_visited": ["Paris", "Lile", "Nancy"],
+    "total_visits": 12
+    },
+    {
+    "country": "Denmark",
+    "cities_visited": ["Copenhagen", "Odense", "Skanderborg"],
+    "total_visits": 12
+    }
+]
+
+print(travel_log3)
