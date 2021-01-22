ba579b17047d612e93e22aa8f6b64834c6c94617
FredTheNinja <fredtheninja@icloud.com>
2021-01-22 13:25:24 +0100

    Day10 Prj_Calc Start

--- Day10/main.py
+++ Day10/main.py
@@ -29,7 +29,8 @@ def format_name(f_name, l_name):
   if f_name == "" or l_name == "":
     return "You didn't provide valid inputs."
   formated_f_name = f_name.title()
-  formated_l_name = l_name.title()
+formated_l_name = l_name.title()
   return f"Result: {formated_f_name} {formated_l_name}"
 
-
+# Testing docstring
+# format_name
