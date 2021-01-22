214d51b84820be690fc7846c7027a6eb0dccbe75
FredTheNinja <fredtheninja@icloud.com>
2021-01-22 13:02:40 +0100

    Day10 Exc1 Done

--- Day10/Exc1/main.py
+++ Day10/Exc1/main.py
@@ -2,16 +2,28 @@ def is_leap(year):
   if year % 4 == 0:
     if year % 100 == 0:
       if year % 400 == 0:
-        print("Leap year.")
+        return True
       else:
-        print("Not leap year.")
+        return False
     else:
-      print("Leap year.")
+      return True
   else:
-    print("Not leap year.")
+    return False
 
-def days_in_month():
-  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
+def days_in_month(in_year, in_month):
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if in_month == 2:
+        if is_leap(in_year) == True:
+            month_days[1] += 1
+            return month_days[in_month-1]
+        elif is_leap(in_year) == False:
+            return(month_days[in_month-1])
+    else:
+        return(month_days[in_month-1])
+
+
+
+
   
   
 #🚨 Do NOT change any of the code below 
