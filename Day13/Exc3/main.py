80732ee3d7955cf63b60d933bf932f4be7d1e5a9
Fred the Ninja <fredtheninja@icloud.com>
2021-01-25 15:01:10 +0100

    Dat13 Exc3 Done

--- Day13/Exc3/main.py
+++ Day13/Exc3/main.py
@@ -1,9 +1,14 @@
+
 for number in range(1, 101):
-  if number % 3 == 0 or number % 5 == 0:
+# Changed if state from or to and since both conditions must resolve to true
+  if number % 3 == 0 and number % 5 == 0:
     print("FizzBuzz")
-  if number % 3 == 0:
+# Changed the if to elif
+  elif number % 3 == 0:
     print("Fizz")
-  if number % 5 == 0:
+# Changed the if to elif
+  elif number % 5 == 0:
     print("Buzz")
   else:
-    print([number])
+# Changed [number] to number for a cleaner look
+    print(number)
